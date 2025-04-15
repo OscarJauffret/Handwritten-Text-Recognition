94ee1cdefe067ced80e853a47ba854ddf0b1a5b5
Oscar Jauffret <oscarjauffret@gmail.com>
2025-04-15 20:06:52 +0200

    Update script documentation to clarify splitting of words and lines

--- code/split_images.py
+++ code/split_images.py
@@ -1,10 +1,10 @@
 """
-The purpose of this script is to split an image into multiple images based on the lines in the image.
-Each line in the image will be saved as a separate image.
+The purpose of this script is to split an image into multiple images based on the lines or words in the image.
+Each line or word in the image will be saved as a separate image.
 """
 
 from config import Config
-from code.extract_bounds import get_line_bounds, get_words_bounds
+from extract_bounds import get_line_bounds, get_words_bounds
 from skimage.io import imread, imsave
 from skimage.transform import resize
 from tqdm import tqdm
